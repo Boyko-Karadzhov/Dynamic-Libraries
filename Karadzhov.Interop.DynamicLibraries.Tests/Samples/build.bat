0c60a87b02e7458bf657a0af3d26cd2c0b8755bc
Boyko-Karadzhov <bokovskii@gmail.com>
2014-11-12 22:50:48 +0200

    removed dll's from samples

---
+++ Karadzhov.Interop.DynamicLibraries.Tests/Samples/build.bat
@@ -0,0 +1,5 @@
+cl delayed_sum.c /link /DLL /OUT:bin\delayed_sum.dll
+cl double_mul.c /link /DLL /OUT:bin\double_mul.dll
+cl double_sum.c /link /DLL /OUT:bin\double_sum.dll
+cl stdcall_sum.c /link /DLL /OUT:bin\stdcall_sum.dll
+cl stdcall_sum.c /link /DLL /OUT:bin\stdcall_sum.dll
